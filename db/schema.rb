84d58fec406ece5b8d936b577e21bd88705b092e
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-10-01 22:23:09 -0400

    add item model, migrations, associations, and seed data

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_02_011255) do
+ActiveRecord::Schema.define(version: 2020_10_02_020858) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.string "image"
+    t.bigint "store_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["store_id"], name: "index_items_on_store_id"
+  end
 
   create_table "stores", force: :cascade do |t|
     t.string "name"
@@ -26,4 +36,5 @@ ActiveRecord::Schema.define(version: 2020_10_02_011255) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "items", "stores"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -23,3 +23,15 @@ kkQuenchers = Store.create(
 	store_link: "https://www.facebook.com/kkquenchers",
 	logo: "https://scontent-mia3-2.xx.fbcdn.net/v/t1.0-9/117434834_106798467804646_1685484019218922180_o.jpg?_nc_cat=110&_nc_sid=09cbfe&_nc_ohc=RjC1-67MT5UAX_hO_Yb&_nc_ht=scontent-mia3-2.xx&oh=5d541eb633a3e60a2bb2e8d455cbeae0&oe=5F9A7866"
 )
+
+Item.create(name: "Kimchi", price: 135, image: "https://scontent-mia3-2.xx.fbcdn.net/v/t1.0-9/119963643_147186503742096_8024949979478927797_n.jpg?_nc_cat=107&_nc_sid=8bfeb9&_nc_ohc=PcvvfMm0IqkAX9OHDXY&_nc_ht=scontent-mia3-2.xx&oh=16aeaa1a82aa8550855a172131772dac&oe=5F9AC8D4", store: craveEat)
+
+Item.create(name: "Rabokki", price: 155, image: "https://scontent-mia3-2.xx.fbcdn.net/v/t1.0-9/120107163_149790333481713_857312582108436178_n.jpg?_nc_cat=105&_nc_sid=e3f864&_nc_ohc=r_BE544ZtoYAX9jJutn&_nc_ht=scontent-mia3-2.xx&oh=32f0785f93b1e474519e2db238a7b4e2&oe=5F9D2285", store: craveEat)
+
+Item.create(name: "Japchae", price: 130, image: "https://scontent-mia3-2.xx.fbcdn.net/v/t1.0-9/120201421_149790020148411_4028257714230480952_n.jpg?_nc_cat=105&_nc_sid=8bfeb9&_nc_ohc=Ob0SDje-NoIAX8FKRWP&_nc_ht=scontent-mia3-2.xx&oh=9f5bcb1869ec8955b8ee008f35a2eff8&oe=5F9A2DBD", store: craveEat)
+
+Item.create(name: "Blueberry Lemonade(16oz)", price: 55, image: "https://scontent-mia3-2.xx.fbcdn.net/v/t1.0-9/118775742_125371309280695_7714891086080541962_n.jpg?_nc_cat=102&_nc_sid=8bfeb9&_nc_ohc=nzYb8wxeAJcAX_o9s-3&_nc_ht=scontent-mia3-2.xx&oh=27a079d60c69c6afa09a88121aafe8df&oe=5F9A2D91", store: kkQuenchers)
+
+Item.create(name: "Classic Lemonade(bottled)", price: 50, image: "https://scontent-mia3-2.xx.fbcdn.net/v/t1.0-9/118663952_125371242614035_3424461243699212231_n.jpg?_nc_cat=110&_nc_sid=8bfeb9&_nc_ohc=2HYgi9ZNm_oAX8JM79W&_nc_ht=scontent-mia3-2.xx&oh=d4cde3bf7b6a2c473ea84bc554cd6ee5&oe=5F9D2A6A", store: kkQuenchers)
+
+Item.create(name: "Classic Lemonade(16oz)", price: 39, image: "https://scontent-mia3-1.xx.fbcdn.net/v/t1.0-9/118663952_125371262614033_426351621499884771_n.jpg?_nc_cat=100&_nc_sid=8bfeb9&_nc_ohc=gvuB6WW9sRcAX-R12l_&_nc_ht=scontent-mia3-1.xx&oh=b9cc41948bc47433b7cf3c209e2f3b74&oe=5F9ADFFC", store: kkQuenchers)
