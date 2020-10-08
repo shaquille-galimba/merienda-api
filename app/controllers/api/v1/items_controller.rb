e18ec58085ced1b16d9c230dc04b130a8b394098
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-10-07 21:56:56 -0400

    add items controller and create action

---
+++ app/controllers/api/v1/items_controller.rb
@@ -0,0 +1,17 @@
+class Api::V1::ItemsController < ApplicationController
+
+	def create
+		item = Item.new(item_params)
+		if item.save
+			render json: ItemSerializer.new(item), status: :accepted
+		else
+			render json: {errors: item.errors.full_messages}, status: :unprocessible_entity
+		end
+	end
+
+	private
+
+	def item_params
+		params.require(:item).permit(:name, :price, :image, :description, :store_id)
+	end
+end
