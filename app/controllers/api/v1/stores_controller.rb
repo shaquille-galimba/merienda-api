73963c579313708db5eb1ec5c463486c40e17c74
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-10-14 16:26:35 -0400

    add create store controller action and routes

--- app/controllers/api/v1/stores_controller.rb
+++ app/controllers/api/v1/stores_controller.rb
@@ -5,4 +5,19 @@ class Api::V1::StoresController < ApplicationController
 
 		render json: StoreSerializer.new(stores)
 	end
+
+	def create
+		store = Store.new(store_params)
+		if store.save
+			render json: StoreSerializer.new(store), status: :accepted
+		else
+			render json: {errors: store.errors.full_messages}, status: :unprocessible_entity
+		end
+	end
+
+	private
+
+	def store_params
+		params.require(:store).permit(:name, :owner, :delivery_location, :store_link, :logo, :description)
+	end
 end
